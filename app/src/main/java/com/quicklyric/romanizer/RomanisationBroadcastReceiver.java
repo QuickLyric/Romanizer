ab986d45d108880a1150a7ae36e8bb0a9883b352
geecko <H.guillaume@gmail.com>
2017-07-01 05:08:12 +0200

    Change input name and exit when finished

--- app/src/main/java/com/quicklyric/romanizer/RomanisationBroadcastReceiver.java
+++ app/src/main/java/com/quicklyric/romanizer/RomanisationBroadcastReceiver.java
@@ -1,7 +1,6 @@
 package com.quicklyric.romanizer;
 
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 
@@ -23,25 +22,23 @@ import android.content.Intent;
 public class RomanisationBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        String[] inputs = intent.getExtras().getStringArray("input");
-        String caller = intent.getExtras().getString("caller_package");
-        String callerClass = intent.getExtras().getString("caller_class");
+        String[] inputs = intent.getExtras().getStringArray("inputs");
 
         if (inputs != null) {
             String[] outputs = new String[inputs.length];
             for (int i = 0; i < inputs.length; i++) {
                 outputs[i] = RomanizerUtil.romanize(inputs[i]);
             }
-            broadcast(context, outputs, caller, callerClass);
+            broadcast(context, outputs);
         }
-        broadcast(context, null, caller, callerClass);
+        broadcast(context, null);
     }
 
-    private void broadcast(Context context, String[] output, String caller, String callerClass) {
-        Intent romanizedIntent = new Intent();
-        romanizedIntent.setComponent(new ComponentName(caller, callerClass));
-        romanizedIntent.setAction("com.geecko.QuickLyric.ROMANIZED_OUTPUT");
-        romanizedIntent.putExtra("output", output);
-        context.sendBroadcast(romanizedIntent);
+    private void broadcast(Context context, String[] output) {
+        Intent romanisedIntent = new Intent();
+        romanisedIntent.setAction("com.geecko.QuickLyric.ROMANIZED_OUTPUT");
+        romanisedIntent.putExtra("output", output);
+        context.sendBroadcast(romanisedIntent);
+        System.exit(0);
     }
 }
